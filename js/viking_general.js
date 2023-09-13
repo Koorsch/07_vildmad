3aa072416df4dae47d5ca8e9ea19408136d3cb16
“Kristine <krho0001@stud.kea.dk>
2023-09-13 21:15:24 +0200

    Ændringer i layout

--- js/viking_general.js
+++ js/viking_general.js
@@ -19,3 +19,15 @@ toggle.addEventListener("click", () => {
 });
 
 
+//OPSKRIFTER
+
+// Hide all elements with class="containerTab", except for the one that matches the clickable grid column
+
+function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
